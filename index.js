8799ee846478741182b9c18bdf065987a13d9d3b
Sledger <beefy1875@hotmail.co.uk>
2023-06-05 14:41:04 +0800

    ex 3.13 fetch from db

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()   
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+
+const Person = require('./models/person')
 
 const app = express()
 
@@ -13,47 +16,20 @@ morgan.token('type', function(req, res) {
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
-
-let persons = [
-    { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-    },
-    { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-    },
-    { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-    },
-    { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-    }
-]
-
 app.get('/', (request, response) => {
     response.send('<h1>Hell owrld </h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id =  Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
+    Person.findById(request.params.id).then(person => {
         response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -85,18 +61,20 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    const newPerson = {
+    const newPerson = new Person({
         name: person.name,
         number: person.number,
         id: newId
-    }
+    })
 
-    persons = persons.concat(newPerson)
-    response.json(newPerson)
+    newPerson.save().then(savedPerson => {
+        response.json(newPerson)
+    })
+
 })
 
 app.get('/api/info', (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} people<br/>
+    response.send(`Phonebook has info for {Person.length} people<br/>
         ${new Date()}`)
 })
 
