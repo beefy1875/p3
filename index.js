c8bf37b9434fb9ca3fadb050e8195a62a494eeb8
Sledger <beefy1875@hotmail.co.uk>
2023-06-06 14:18:57 +0800

    ex 3.17

--- index.js
+++ index.js
@@ -4,18 +4,32 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 const Person = require('./models/person')
+const person = require('./models/person')
 
 const app = express()
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if(error.name === 'CastError') {
+        return response.status(400).send({error: 'malformed id'})
+    }
+
+    next(error)
+}
 
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
+
 
 morgan.token('type', function(req, res) {
     return JSON.stringify(req.body)
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
+
 app.get('/', (request, response) => {
     response.send('<h1>Hell owrld </h1>')
 })
@@ -32,12 +46,14 @@ app.get('/api/persons/:id', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndRemove(request.params.id)
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
         .then(result => {
-            response.status(204).end()
+            console.log('a')
         })
-//        .catch(error => next(error))
+        .catch(error => {
+            next(error)
+        })
 
 })
 
@@ -73,12 +89,35 @@ app.post('/api/persons', (request, response) => {
 
 })
 
+app.put('/api/persons/:id', (req, response, next) => {
+    console.log('putting')
+
+    const body = req.body
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    console.log(person)
+        
+    Person.findByIdAndUpdate(req.params.id, person, {new: true})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+
 app.get('/api/info', (request, response) => {
     response.send(`Phonebook has info for {Person.length} people<br/>
         ${new Date()}`)
 })
 
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log((`Server running on port ${PORT}`))
 })
+
+
